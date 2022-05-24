96b732a88c3e850e630f229a0b7dfff18b3fd8be
TuanAnhNguyen14111998 <tuananh14111998@gmail.com>
2022-05-24 23:36:45 +0700

    Complete code training model and tracked model weights

--- starter/starter/ml/model.py
+++ starter/starter/ml/model.py
@@ -1,3 +1,4 @@
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 
 
@@ -17,8 +18,10 @@ def train_model(X_train, y_train):
     model
         Trained machine learning model.
     """
-
-    pass
+    rfc_model = RandomForestClassifier()
+    rfc_model.fit(X_train, y_train)
+    
+    return rfc_model
 
 
 def compute_model_metrics(y, preds):
@@ -40,6 +43,7 @@ def compute_model_metrics(y, preds):
     fbeta = fbeta_score(y, preds, beta=1, zero_division=1)
     precision = precision_score(y, preds, zero_division=1)
     recall = recall_score(y, preds, zero_division=1)
+
     return precision, recall, fbeta
 
 
@@ -48,7 +52,7 @@ def inference(model, X):
 
     Inputs
     ------
-    model : ???
+    model : Random Forest Classifier model
         Trained machine learning model.
     X : np.array
         Data used for prediction.
@@ -57,4 +61,6 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    pass
+    preds = model.predict(X)
+
+    return preds
--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -1,13 +1,17 @@
 # Script to train machine learning model.
-
+# Add the necessary imports for the starter code.
+import pandas as pd
 from sklearn.model_selection import train_test_split
 
-# Add the necessary imports for the starter code.
+from ml.data import process_data
+from ml.model import train_model, inference, compute_model_metrics
+import pickle
 
 # Add code to load in the data.
+data = pd.read_csv('../data/census_clean.csv')
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = train_test_split(data, test_size=0.20)
+train, test = train_test_split(data, test_size=0.20, random_state=42)
 
 cat_features = [
     "workclass",
@@ -24,5 +28,30 @@ X_train, y_train, encoder, lb = process_data(
 )
 
 # Proces the test data with the process_data function.
+X_test, y_test, encoder, lb = process_data(
+    test, categorical_features=cat_features, label="salary", training=False, encoder=encoder, lb=lb
+)
 
-# Train and save a model.
+# Train and save a model
+rfc_model = train_model(X_train=X_train, y_train=y_train)
+
+# Get Predictions and model metrics 
+preds = inference(model=rfc_model, X=X_test)
+precision, recall, fbeta = compute_model_metrics(y=y_test, preds=preds)
+print("Precision: ", precision)
+print("Recall: ", recall)
+print("FBeta: ", fbeta)
+
+with open('../model/rfc_model.pkl', 'wb') as pickle_file:
+    pickle.dump(rfc_model, pickle_file)
+
+with open('../model/encoder.pkl', 'wb') as pickle_file:
+    pickle.dump(encoder, pickle_file)
+
+with open('../model/lb.pkl', 'wb') as pickle_file:
+    pickle.dump(lb, pickle_file)
+
+with open('../model/metrics.txt', "w") as metrics_file:
+    metrics_file.write(f"Precision: {precision}\n")
+    metrics_file.write(f"Recall: {recall}\n")
+    metrics_file.write(f"FBeta: {fbeta}\n")
